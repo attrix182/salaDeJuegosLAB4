045c5518801cc9f72fcc44cb4326edee34124e0c
attrix <lucho.bernal@hotmail.com>
2021-03-26 00:25:00 -0300

    heroku

---
+++ salaDeJuegos/src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const path = require('path');
+
+app.use(express.static(__dirname + '/salaDeJuegos'));
+
+app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req, res) {
+
+    res.sendFile(path.join(__dirname + '/src/index.html'));
+});
+
+console.log("Build");
