45b18890109dc8464ed61cf7eaffee1c4659e87a
attrix <lucho.bernal@hotmail.com>
2021-03-29 23:45:17 -0300

    firebase

--- salaDeJuegos/src/environments/environment.prod.ts
+++ salaDeJuegos/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAdLRPswxk-NHQMaozMMlgAI_YjkdWSRkE",
+    authDomain: "tpsaladejuegosutn.firebaseapp.com",
+    databaseURL: "https://tpsaladejuegosutn-default-rtdb.firebaseio.com",
+    projectId: "tpsaladejuegosutn",
+    storageBucket: "tpsaladejuegosutn.appspot.com",
+    messagingSenderId: "544710299304",
+    appId: "1:544710299304:web:f29ba8f44cdcdc9932fc9d"
+  }
 };
